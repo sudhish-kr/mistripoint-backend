a392f4e91d2138f823a1400122780d37a3769611
sudhish-kr <sudhish.techcroon@gmail.com>
2026-06-19 21:11:46 +0530

    Use SQLite

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Arjunpandat%4052@192.168.1.21:3306/mistripoints"
+DATABASE_URL = "sqlite:///./mistripoint.db"
 
 engine = create_engine(DATABASE_URL)
 
