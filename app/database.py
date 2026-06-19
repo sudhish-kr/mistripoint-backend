f3d6682a358271ee16eac0149689d24a311f11aa
sudhish-kr <sudhish.techcroon@gmail.com>
2026-06-19 18:54:05 +0530

    Customer management module completed

--- app/database.py
+++ app/database.py
@@ -7,11 +7,9 @@ engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
-
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
 Base = declarative_base()
